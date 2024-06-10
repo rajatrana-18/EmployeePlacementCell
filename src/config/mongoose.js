d005b39044a655f627308073e31bbf672a681bad
Rajat Rana <130746911+rajatrana-18@users.noreply.github.com>
2024-06-11 00:49:57 +0530

    Add files via upload

---
+++ src/config/mongoose.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+// mongodb connection url
+// const url = "mongodb://localhost:27017/employeeDB";
+// const url = "mongodb+srv://rajatrana000018:UajPjSNUvr0wFZra@cluster0.b2jnaoe.mongodb.net/employeeDB?retryWrites=true&w=majority&appName=Cluster0";
+const url = process.env.DB_URL;
+// connect to mongodb using mongoose
+export const connectUsingMongoose = async () => {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Mongodb connected using mongoose");
+    } catch (err) {
+        // log an error msg if the connection fails.
+        console.log("Error while connecting to db");
+        console.log(err);
+    }
+}
